8450ca57839db9fdfba38fa22a28c5b413b7b340
Kshitij Yelpale <kshitij.yelpale@gmail.com>
2019-11-29 00:30:25 +0100

    Insulin pump Spring boot project start

---
+++ insulin-pump/src/main/java/com/scs/insulinpump/insulinpump/InsulinPumpApplication.java
@@ -0,0 +1,13 @@
+package com.scs.insulinpump.insulinpump;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InsulinPumpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InsulinPumpApplication.class, args);
+	}
+
+}
---
+++ insulin-pump/src/test/java/com/scs/insulinpump/insulinpump/InsulinPumpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.scs.insulinpump.insulinpump;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InsulinPumpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
