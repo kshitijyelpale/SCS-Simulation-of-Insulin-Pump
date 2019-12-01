4d523228f46e5d6c349a936d7723fa75fe346620
Kshitij Yelpale <kshitij.yelpale@gmail.com>
2019-12-01 16:23:30 +0100

    (+) added sample controller file

--- insulin-pump/src/main/java/com/scs/insulinpump/InsulinPumpApplication.java
+++ insulin-pump/src/main/java/com/scs/insulinpump/InsulinPumpApplication.java
@@ -1,4 +1,4 @@
-package com.scs.insulinpump.insulinpump;
+package com.scs.insulinpump;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
