a938189a3b9a97ebebbf72fa1c29d0cc354b981f
Kshitij Yelpale <kshitij.yelpale@gmail.com>
2019-12-07 18:45:56 +0100

    removedt he sample test code

---
+++ Sample Test Code/src/main/java/repository/UserRepository.java
@@ -0,0 +1,11 @@
+package repository;
+
+import entity.User;
+import org.springframework.data.repository.CrudRepository;
+
+// This will be AUTO IMPLEMENTED by Spring into a Bean called userRepository
+// CRUD refers Create, Read, Update, Delete
+
+public interface UserRepository extends CrudRepository<User, Integer> {
+
+}
