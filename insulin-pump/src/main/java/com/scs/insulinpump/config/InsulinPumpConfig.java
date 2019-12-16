b1e0cc95221e297979b2dd4cd0b510cc26b28647
Kshitij Yelpale <kshitij.yelpale@gmail.com>
2019-12-16 17:35:20 +0100

    (+) added bean of Patient class in configuration class

--- insulin-pump/src/main/java/com/scs/insulinpump/config/InsulinPumpConfig.java
+++ insulin-pump/src/main/java/com/scs/insulinpump/config/InsulinPumpConfig.java
@@ -1,9 +1,16 @@
 package com.scs.insulinpump.config;
 
+import com.scs.insulinpump.domain.Patient;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @Configuration
 @EnableSwagger2
 public class InsulinPumpConfig {
+
+    @Bean
+    public Patient patient() {
+        return new Patient();
+    }
 }
