b4cea13e7b3a1e147be9b41bb7c4e5b1960aed4b
Kshitij Yelpale <kshitij.yelpale@gmail.com>
2020-02-09 02:25:13 +0100

    Emergency contacts
    (+) added div in html file of contacts
    (*) changed the save post call with contacts
    (+) added new instance variable for User and UserDTO
    (*) changed register method to store contacts of the user

--- inProject/src/main/java/com/inglpump/service/UserService.java
+++ inProject/src/main/java/com/inglpump/service/UserService.java
@@ -119,6 +119,7 @@ public class UserService {
         Set<Authority> authorities = new HashSet<>();
         authorityRepository.findById(AuthoritiesConstants.USER).ifPresent(authorities::add);
         newUser.setAuthorities(authorities);
+        newUser.setEmergencyContacts(userDTO.getEmergencyContacts());
         userRepository.save(newUser);
         this.clearUserCaches(newUser);
         log.debug("Created Information for User: {}", newUser);
